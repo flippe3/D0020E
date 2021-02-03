efc41f202fa9a6134bcacc528aecbf2731aa0c3a
Tobias <tobhei-8@student.ltu.se>
2021-02-03 19:44:24 +0100

    Added comments

--- OauthDev/OAuth.py
+++ OauthDev/OAuth.py
@@ -1,26 +1,37 @@
-from typing import re
-
-import development.NetworkHandler as networkHandler
-import os, binascii, random, time,json
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from http.client import HTTPConnection
+import binascii
+import json
+import os
+import time
 from urllib.parse import parse_qs, urlparse
 
-# assumptions: Adding a discovery giving out ClientID and setting clienttype to confidential
+import development.NetworkHandler as networkHandler
+
+#This file handles ServerSide Oauth authentication, to use it first send a get request to '/discovery' to get a userId
+# as a responce . This gives you the required information to request a authenficaion token by GET requesting '/authorize'
+# and sending 'response_type', 'client_id', 'state'(generated yourself) as parameters in the url.
+# This will give u 10 minutes of a usable access token as a responce.
+
+#This class also contains a basic example of server side Token handeling look at '/token' as a post Request
+
+# assumptions: Adding a discovery giving out ClientID
 # Authorization response: responces instead of redirectiong
 # 1 year expiery på accsess token
 sessions = []
 tokens = []
 
 
+
+
 class OAuth:
 
+    #Generates a session key for a oauth session
     def generate_session(self):
         cid = self.generate_client_ID()
         s = OAuth.Session(cid)
         sessions.append(s)
         return s
 
+    #generates a Unique Client id
     def generate_client_ID(self):
         cid = ''
         b = True
@@ -32,11 +43,14 @@ class OAuth:
                     b = True
         return cid
 
+    #Generates a random base 64 string
     def generate_state(self):
         cid = binascii.b2a_base64(os.urandom(32))
         cid = cid.decode("ascii").replace('=\n','')
         return cid
 
+
+    #Holds data around a Oauth Authentification event TODO: might want to save accross restars
     class Session:
         def __init__(self, cid):
             self.clientID = cid
@@ -44,17 +58,22 @@ class OAuth:
             self.code = ""
             self.expire = 0
 
+        #generates the code used for the 'Code' parameter in Oauth authentification
         def generate_code(self):
-            self.expire = time.time() + 10 * 1000 * 60
+            self.expire = time.time() + 10 * 1000 * 60 #10 minutes
             code = binascii.b2a_base64(os.urandom(128)).decode('ascii').replace('=\n', '')
             print("code: " + code)
             self.code = code
             return code
 
+        #Verifys that a token request contains a valid key
         def verify_valid_code(self, code):
             return self.expire > time.time() and code == self.code
 
+    #This is a Http Server configured for responding to Oauth requests
     class OAuthServer(networkHandler.Server):
+
+        #All GET requests will go here
         def do_GET(self):
             super().do_GET()
             o = urlparse(self.path)
@@ -65,7 +84,7 @@ class OAuth:
                 self.discovery(query)
 
 
-
+        #All Post requests goes here
         def do_POST(self):
             super().do_POST()
             o = urlparse(self.path)
@@ -74,13 +93,15 @@ class OAuth:
                 self.token(query)
 
 
-
+        #handels the get discovery, and thereby gives the user its user ID #TODO does this want to be a siged message by Agent?
         def discovery(self, query):
             self.response()
             s = OAuth().generate_session()
             print("cid:" + s.clientID)
             self.wfile.write(bytes(s.clientID, "utf-8"))
 
+
+        #once a client has its User id it can request its code, This code valides the userid
         def authorize(self, query):
             print("authorizing")
             if query.get("response_type")[0] == "code":
@@ -99,6 +120,7 @@ class OAuth:
                 print("invalid response type:")
                 print(query.get("response_type"))
 
+        #the responce the client will recive on a authorize request
         def authorize_response(self, session):
             self.response()
 
@@ -110,6 +132,8 @@ class OAuth:
             self.wfile.write(bytes(encoded, "utf-8"))
             # self.wfile.write(bytes("code:" + code, "utf-8"))
 
+
+        #once a request containg a userid is sent this will find the relevant session for that data to be extraced
         def find_session(self, cid):
             print("findning client id")
             for x in sessions:
@@ -117,6 +141,8 @@ class OAuth:
                     return x
             return None
 
+
+        #this validates the code sent out in authorize
         def token(self, query):
             if query.get("grant_type")[0] == "authorization_code":
                 cid = query.get("client_id")[0]
@@ -132,6 +158,7 @@ class OAuth:
                 print("wrong grant type")
                 print(query.get("grant_type")[0])
 
+        #this is a simple example for how a token could be sent out, Will be Replaced by the PoA itself
         def token_response(self, query):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -141,6 +168,7 @@ class OAuth:
             self.wfile.write(bytes(token.toJson(), "utf-8"))
 
 
+#Simple Accsess token example
 class Token:
     def __init__(self, host):
         oauth = OAuth()

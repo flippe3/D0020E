4393751f0197e7c5386904fb840faa593288bf12
EmilWik <rock99@live.se>
2021-02-04 09:52:32 +0100

    Ändra now() till utcnow()

--- OauthDev/Util.py
+++ OauthDev/Util.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 def getNow():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")      # <--- Visst ville vi använda utcnow() istället för now()?

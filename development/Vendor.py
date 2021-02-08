d911e04b9d29d1647a1ed4b7f094665300fe2726
Tobias <tobhei-8@student.ltu.se>
2021-02-08 11:07:55 +0100

    Vendor run fix

--- development/Vendor.py
+++ development/Vendor.py
@@ -11,6 +11,45 @@ import Constants as CONSTS
 
 #data_string = '{ "poa" :"' + Constants.valid_token + '"}'
 
+import datetime  # Only used to check 'exp', but 'exp' is already checked during the decoding so
+import Register  # Behövs inte än. Ska fixa register
+
+
+class Verifier:
+
+    def verify_poa(self, payload):
+        try:
+            agent_public_key = payload["agent_public_key"]
+            principal_public_key = payload["principal_public_key"]
+            resource_owner_id = payload["resource_owner_id"]
+            exp = payload["exp"]
+
+            return Verifier.verify_payload(self, agent_public_key, principal_public_key, resource_owner_id, exp)
+
+        except KeyError:
+            print("Payload is missing mandatory data")
+            return False
+
+    def verify_payload(self, agent_public_key, principal_public_key, resource_owner_id, exp):
+
+        # Check keys
+        if (agent_public_key != CONSTS.agent_public_key):
+            print("Incorrect agent_public_key")
+            return False
+        if (principal_public_key != CONSTS.principal_public_key):
+            print("Incorrect principal_public_key")
+            return False
+        if (resource_owner_id != CONSTS.vendor_public_key):
+            print("Incorrect vendor_public_key")
+            return False
+
+        if (exp < datetime.datetime.utcnow().timestamp()):  # This is automatically checked when POA gets decoded
+            print("POA has expired")
+            return False
+
+        return True
+
+
 class VendorServer(nh.Server):
 
     def do_GET(self):
@@ -31,61 +70,25 @@ class VendorServer(nh.Server):
             print("Successfully decoded PoA:", decoded_poa)
         except:
             print("Vendor failed to decode the PoA") # This will happen if poa is expired btw
+            self.wfile.write("PoA NOT DECODED And theirfor can not be used".encode("utf-8"))
 
 
         print("Vendor verifying PoA")
         
 
         if (Verifier().verify_poa(decoded_poa)):
-            print("PoA verfied.")
-            self.wfile.write("PoA Verified".encode("utf-8"))
+            print("PoA Verified And Used.")
+            self.wfile.write("PoA Use Granted And Verified for use".encode("utf-8"))
             # PoA is verified successfully.
             # Here a message should be sent to the Agent show that the PoA verified.
         else:
             print("Vendor unsuccessfully verified the PoA.")
+            self.wfile.write("PoA Use NOT ALLOWED".encode("utf-8"))
 
     
         # REMOVE ABOVE COMMENT WHEN VERFIER WORKS (and remove the code beneath), this is just for demo.
         
         #self.wfile.write(bytes("POA USE GRANTED", "utf-8"))
-
-
-import datetime  # Only used to check 'exp', but 'exp' is already checked during the decoding so 
-import Register  # Behövs inte än. Ska fixa register
-class Verifier:
-
-    def verify_poa(self, payload):
-        try:
-            agent_public_key        = payload["agent_public_key"]
-            principal_public_key    = payload["principal_public_key"]
-            resource_owner_id       = payload["resource_owner_id"]
-            exp                     = payload["exp"]    
-
-            return Verifier.verify_payload(self, agent_public_key, principal_public_key, resource_owner_id, exp)      
-            
-        except KeyError:
-            print("Payload is missing mandatory data")
-            return False
-    
-    
-    def verify_payload(self, agent_public_key, principal_public_key, resource_owner_id, exp):
-
-        # Check keys
-        if(agent_public_key     != CONSTS.agent_public_key):
-                print("Incorrect agent_public_key")
-                return False
-        if(principal_public_key != CONSTS.principal_public_key):
-                print("Incorrect principal_public_key")
-                return False
-        if(resource_owner_id    != CONSTS.vendor_public_key):
-                print("Incorrect vendor_public_key")
-                return False
-
-        if(exp < datetime.datetime.utcnow().timestamp()):   # This is automatically checked when POA gets decoded 
-                print("POA has expired")
-                return False
-
-        return True
 
 
 

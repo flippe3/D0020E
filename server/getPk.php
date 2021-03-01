30c02dbb4af304eecd3a31386dfcf0f8e7be3e34
Filip Nilsson <filip.c.nilsson@gmail.com>
2021-03-01 16:46:50 +0000

    changing get to post, work in progress

--- server/getPk.php
+++ server/getPk.php
@@ -1,23 +1,30 @@
 <?php
-if (isset($_GET['ID']) && $_GET['ID']!="") {
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+$search_val = $data->search_value;
+
+if (isset($search_val) && $search_val != "") {
  $link = mysqli_connect("localhost", "root", "hackerman", "keyregistry");
- $id = $_GET['ID'];
+ $id = $search_val;
+
  $request = "SELECT * FROM users WHERE ID='$id'";
  $result = mysqli_query($link, $request);
  if(mysqli_num_rows($result)>0)
  {
  $row = mysqli_fetch_assoc($result);
  $pk = $row['PublicKey'];
- #echo $pk;
  $name = $row['Name'];
  response($id, $pk, $name, NULL, NULL);
  mysqli_close($link);
- } else {
+ }
+ else {
    echo "No record found<br>";
    response($id, NULL, NULL, 200,"No Record Found");
-   }
-}else{
-   response($id, NULL, NULL, 400,"Invalid Request");
+}
+}
+else{
+   echo "Invalid request";
+   #response($id, NULL, NULL, 400,"Invalid Request");
 }
 
 function response($id,$pk,$name, $code, $msg){
@@ -25,6 +32,9 @@ function response($id,$pk,$name, $code, $msg){
  $response['PublicKey'] = $pk;
  $response['Name'] = $name; 
  $json_response = json_encode($response);
- echo $json_response;
+ #echo $json_response;
+ if ($code == NULL){
+    echo $pk;
+ }
 }
 ?>

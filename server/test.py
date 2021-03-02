6ac2a4874f51b9235f2da0661a70ce6f9383b15d
EmilWik <rock99@live.se>
2021-03-02 19:03:19 +0100

    Formated print out
    to make it less clutered

--- server/test.py
+++ server/test.py
@@ -3,7 +3,7 @@ import requests
 url = 'http://fnilsson.com/getPk/'
 data = {"search_value": "1"}
 
-print("Post request to get PublicKey")
+print("\nPost request to get PublicKey:")
 get_pk = requests.post(url, json=data)
 
 pk = get_pk.text
@@ -15,7 +15,7 @@ print(pk)
 url2 = 'http://fnilsson.com/getUserId/'
 data2 = {"search_value": pk}
 
-print("Post request to get ID with PublicKey")
+print("\nPost request to get ID with PublicKey:")
 get_id = requests.post(url2, json=data2)
 
 print(get_id)
@@ -24,7 +24,7 @@ print(get_id.text)
 url3 = 'http://fnilsson.com/verifyPk/'
 data3 = {"search_value": pk}
 
-print("Post request to verify PublicKey")
+print("\nPost request to verify PublicKey:")
 verify_pk = requests.post(url3, json=data3)
 
 print(verify_pk)
@@ -33,7 +33,7 @@ print(verify_pk.text)
 url4 = 'http://fnilsson.com/verifyUserId/'
 data4 = {"search_value": get_id.text}
 
-print("Post request to verify userId")
+print("\nPost request to verify userId:")
 verify_id = requests.post(url4, json=data4)
 
 print(verify_id)

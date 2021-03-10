5cfd234bb6f6468e9a997d94545853950ba294e5
fleip <filip.c.nilsson@gmail.com>
2021-03-10 13:07:55 +0100

    hard setting date for exp

--- OauthDev/Poa.py
+++ OauthDev/Poa.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 
 class Poa:
-    def __init__(self, agent_public_key=None, principal_public_key=None, resource_owner_id=None, exp=None, metadata=None, iat=datetime.utcnow().timestamp()-36000, nbf = None):
+    def __init__(self, agent_public_key=None, principal_public_key=None, resource_owner_id=None, exp=datetime.utcnow().timestamp()+360000, metadata=None, iat=datetime.utcnow().timestamp()-36000, nbf = None):
         self.agent_public_key = agent_public_key
         self.principal_public_key = principal_public_key
         self.resource_owner_id = resource_owner_id
-        self.exp = exp
+        self.exp = datetime.utcnow().timestamp()+360000
         self.metadata = metadata
         self.iat = iat
         self.nbf = nbf
--- WebbPages/WebbPageScripts.py
+++ WebbPages/WebbPageScripts.py
@@ -21,7 +21,7 @@ def project(poas, agentID, princId):
         #    a.get("metadata")) + " Iat:" + a.get('iat') + " Exp: " + a.get('exp') + \
         #          "</a> <button onClick=\"clipboard"+str(d)+"()\">Copy To clipbord</button>"
         script+=  "\ndocument.getElementById(\'Items\').innerHTML += \'<div>\'"
-        script+=  "\ndocument.getElementById(\'Items\').innerHTML += \'<a>" + json.dumps(a.get("metadata")) + " Iat:" + datetime.datetime.fromtimestamp( a.get('iat') ).strftime("%Y-%m-%d-%H:%M") + " UTC TIME Exp: " + a.get('exp') + "  AgentID: "+str(agentID) + "  Principal ID: "+str(pid)+"</a>\'"
+        script+=  "\ndocument.getElementById(\'Items\').innerHTML += \'<a>" + json.dumps(a.get("metadata")) + " Iat:" + datetime.datetime.fromtimestamp( a.get('iat') ).strftime("%Y-%m-%d-%H:%M") + " UTC TIME Exp: " + str(a.get('exp')) + "  AgentID: "+str(agentID) + "  Principal ID: "+str(pid)+"</a>\'"
         script+=  "\ndocument.getElementById(\'Items\').innerHTML += \' <textarea id=\"a"+str(d)+"\">"+ poc["poa"] +"</textarea>\'"
         script+=  "\ndocument.getElementById(\'Items\').innerHTML += \'<button onClick=\"clipboard"+str(d)+"()\">Copy To clipbord</button>\'"
         d= d+1

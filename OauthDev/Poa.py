e711fccd9c21bb29d5a4add4ffc50c49d84bd78d
fleip <filip.c.nilsson@gmail.com>
2021-03-10 13:02:27 +0100

    iat date (temporarily hard coded)

--- OauthDev/Poa.py
+++ OauthDev/Poa.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class Poa:
-    def __init__(self, agent_public_key=None, principal_public_key=None, resource_owner_id=None, exp=None, metadata=None, iat=datetime.utcnow().timestamp(), nbf = None):
+    def __init__(self, agent_public_key=None, principal_public_key=None, resource_owner_id=None, exp=None, metadata=None, iat=datetime.utcnow().timestamp()-36000, nbf = None):
         self.agent_public_key = agent_public_key
         self.principal_public_key = principal_public_key
         self.resource_owner_id = resource_owner_id
